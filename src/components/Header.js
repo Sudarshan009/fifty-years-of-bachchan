a4fe9e765d80893170ff009958ac4c3149b7ff9b
Sudarshan009 <sudarshan.mali09@gmail.com>
2020-12-13 13:03:40 +0530

    first

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ export const Header = () => {
         <div className="headerDiv">
            <h1 className="mainTitle">50 Years of Bachchan, 50 Memorable Scenes</h1>
 
-           <Button variant="contained" color="secondary">
+           {/* <Button variant="contained" color="secondary">
   Secondary
 </Button>
 <Button variant="contained" color="secondary">
@@ -18,7 +18,7 @@ export const Header = () => {
 </Button>
 <Button variant="contained" color="secondary">
   Secondary
-</Button>
+</Button> */}
             
             <p className="headerPara">Fifty years ago, a lanky, deep-voiced actor burst onto the screens in a little-seen film named Saat Hindustani. Fifty years later, Hindi cinema seems unimaginable without him. He was the hugest of stars, whose flops reportedly made more money than others’ hits. He was also a great actor, who could go through the gamut. From the gestural to the theatrical, from the internalised to the starry, from the roar to the whisper, from cross-dressing and co-s­­­tarring with a cockroach to narrating a Satyajit Ray epic, he has done it all. His name, of course, is Amitabh Bachchan, and in this article Film Companion recalls 50 of his most memorable scenes.</p>
         </div>
